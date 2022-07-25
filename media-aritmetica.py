a79d0f42fa1c43d83c6365d20926f6ffb46f33e3
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 13:57:53 -0300

    Create media-aritmetica.py

---
+++ media-aritmetica.py
@@ -0,0 +1,8 @@
+# Faça um programa em Python que receba quatro notas, calcule e imprima a média aritmética.
+primeira=float(input("Digite a primeira nota: "))
+segunda=float(input("Digite a segunda nota: "))
+terceira=float(input("Digite a terceira nota: "))
+quarta=float(input("Digite a quarta nota: "))
+print("A média aritmética é",(primeira+segunda+terceira+quarta)/4)
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
